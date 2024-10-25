85b07fa67a4c08b4751ae02e4b7892dd62a1e15a
BedaprakashSaidarsanSahoo <bedaprakash.underpinservices@gmail.com>
2024-10-25 19:35:40 +0530

    Fix Update

--- Assets/Scripts/Functionality/SlotBehaviour.cs
+++ Assets/Scripts/Functionality/SlotBehaviour.cs
@@ -214,14 +214,14 @@ public class SlotBehaviour : MonoBehaviour
         if(currentBalance < currentTotalBet)
         {
             uiManager.LowBalPopup();
-            if (AutoSpin_Button) AutoSpin_Button.interactable = false;
-            if (SlotStart_Button) SlotStart_Button.interactable = false;
+            //if (AutoSpin_Button) AutoSpin_Button.interactable = false;
+            //if (SlotStart_Button) SlotStart_Button.interactable = false;
         }
-        else
-        {
-            if (AutoSpin_Button) AutoSpin_Button.interactable = true;
-            if (SlotStart_Button) SlotStart_Button.interactable = true;
-        }
+        //else
+        //{
+        //    if (AutoSpin_Button) AutoSpin_Button.interactable = true;
+        //    if (SlotStart_Button) SlotStart_Button.interactable = true;
+        //}
     }
 
     internal void FetchLines(string LineVal, int count)
@@ -448,6 +448,7 @@ public class SlotBehaviour : MonoBehaviour
             CompareBalance();
             StopAutoSpin();
             yield return new WaitForSeconds(1);
+            ToggleButtonGrp(true);
             yield break;
         }
 
